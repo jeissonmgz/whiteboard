fad1fde5e1f4663c0d6fc19da3ca18c0973387b4
Jeisson Malaver <jeissonmgz@gmail.com>
2020-03-23 23:02:43 -0500

    Design menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { ControlComponent } from './control/control.component';
+import { ButtonComponent } from './control/button/button.component';
+import { PropertyComponent } from './property/property.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ControlComponent,
+    ButtonComponent,
+    PropertyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
